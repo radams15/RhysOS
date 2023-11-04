63697869d869df3e4d76c553e56157c50b409b0f
Rhys Adams <rhys@therhys.co.uk>
2023-11-04 16:04:48 +0000

    Make time show 12hr clock.

--- programs/time/main.c
+++ programs/time/main.c
@@ -12,7 +12,10 @@ int main(int argc, char** argv) {
 
     close(fh);
 
-    printf("%d:%d:%d:%d\n", time.hr, time.min, time.sec, time.tick);
+    printf("%d:%d:%d:%d %s\n",
+        time.hr > 12? time.hr-12 : time.hr,
+        time.min, time.sec, time.tick,
+        time.hr > 12? "pm" : "am");
 
     return 0;
 }
