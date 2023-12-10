e6f4e1c0cd7b0fce57352fc515e6f7f5d00516e8
Rhys Adams <rhys@therhys.co.uk>
2023-12-10 16:50:06 +0000

    Add Fopen, etc.

--- stdlib/stdlib.c
+++ stdlib/stdlib.c
@@ -1,5 +1,7 @@
 #include "stdlib.h"
+#include "syscall.h"
 #include "stdio.h"
+#include "malloc.h"
 
 void memset(int* buf, int val, int n) {
     for (; n != 0; n--)
@@ -14,4 +16,43 @@ void memcpy(uint8* dst, uint8* src, int n) {
 
 void abort() {
     fprintf(stderr, "Program abort!\n");
+}
+
+File_t* fopen(const char* fname, const char* mode) {
+    FileMode_t mode_val = O_RDONLY;
+
+    int fh = open(fname, mode_val);
+
+    for(char* c=mode ; *c != NULL ; c++) {
+        if(*c == 'w')
+            mode_val |= O_CREAT;
+            mode_val &= ~O_RDONLY;
+        if(*c == 'a')
+            mode_val |= O_APPEND;
+    }
+
+    if(fh == -1) {
+        return NULL;
+    }
+
+    File_t* out = malloc(sizeof(File_t));
+    out->fh = fh;
+
+    return out;
+}
+
+int fwrite(File_t* file, unsigned char* buffer, unsigned int size) {
+    return write(file->fh, buffer, size);
+}
+
+int fclose(File_t* file) {
+    int out = close(file->fh);
+
+    free(file);
+
+    return out;
+}
+
+int fseek(File_t* file, int pos) {
+    return seek(file->fh, pos);
 }
--- stdlib/stdlib.h
+++ stdlib/stdlib.h
@@ -10,7 +10,17 @@ typedef struct TimeDelta {
     int hr;
 } TimeDelta_t;
 
+typedef struct File {
+    int fh;
+} File_t;
+
 void memset(int* buf, int val, int n);
 void memcpy(uint8* dst, uint8* src, int n);
+
+File_t* fopen(const char* fname, const char* mode);
+int fwrite(File_t* file, unsigned char* buffer, unsigned int size);
+int fclose(File_t* file);
+int fseek(File_t* file, int pos);
+
 
 #endif
