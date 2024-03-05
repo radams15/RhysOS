c4da18dc5cbc3302ce6a47f6e30107204900e40d
Rhys Adams <rhys@therhys.co.uk>
2024-03-05 15:11:26 +0000

    Format all files.

--- kernel/malloc.c
+++ kernel/malloc.c
@@ -4,146 +4,111 @@
 #include "type.h"
 #include "util.h"
 
-unsigned int heap_begin_addr;
-unsigned int heap_end_addr;
+extern unsigned int heap_begin;
+extern unsigned int heap_end;
+static unsigned int heap;
 
+#define HEAP_MAGIC 0xCAFE
 
-unsigned char* heap_begin;
-unsigned char* heap_end;
-unsigned char* heap;
-unsigned int heap_size;
+/* Kernel memory allocator.
+ *
+ *
+ * Similar to the original MS-DOS allocator, this algorithm
+ * splits memory into variable-length chunks which can be re-combined
+ * when free'd.
+ * Each block begins with a BlkHeader, which is a linked-list structure
+ * allowing the next block to be pointed to, along with the block length
+ * and whether the block is free.
+ *
+ * Headers begin with a magic variable to allow the kernel to show an error
+ * if a free'd block is not actually a memory block which it allocated.
+ */
 
-const static unsigned char magic = 0xBE;
+typedef struct BlkHeader {
+    unsigned int magic;      // HEAP_MAGIC
+    unsigned int length;     // Length of the block
+    struct BlkHeader* next;  // Pointer to next block
+    unsigned int free;       // Is it being used?
+} BlkHeader_t;
 
-struct BlkHeader {
-    unsigned char magic;
-    unsigned int length;
-    unsigned char* next;
-    unsigned char free;
-};
+int memmgr_init() {
+    heap = (unsigned int)&heap_begin;
 
-void error(const char* msg) {
-    print_string((char*) msg);
-    print_char('\n');
-}
+    BlkHeader_t* header = (BlkHeader_t*)&heap_begin;
 
-int defn_header(unsigned char* ptr, unsigned len, unsigned char* next, unsigned char free) {
-    struct BlkHeader* header = (struct BlkHeader*) ptr;
+    header->magic = HEAP_MAGIC;
+    header->length = &heap_end - &heap_begin;
+    header->free = 1;
 
-    header->magic = magic;
-    header->length = len;
-    header->next = next;
-    header->free = free;
+    header->next = NULL;
 
     return 0;
 }
 
-struct BlkHeader* parse_block(unsigned char* ptr) {
-    struct BlkHeader* header = (struct BlkHeader*) ptr;
+BlkHeader_t* split(BlkHeader_t* block, unsigned int size) {
+    BlkHeader_t* out = block;
 
-    if(ptr == NULL) {
-        error("Invalid block parse (NULL)");
-        return NULL;
-    }
+    int old_size = block->length;
 
-    if(header->magic != magic) {
-        error("Invalid block magic!");
-        return NULL;
-    }
+    block += size + sizeof(BlkHeader_t);
+    block->magic = HEAP_MAGIC;
+    block->length = old_size - (size + sizeof(BlkHeader_t));
+    block->free = 1;
 
-    return header;
-}
-
-struct BlkHeader* split(struct BlkHeader* block, unsigned int size) {
-    struct BlkHeader* out = block;
-
-    unsigned int old_size = block->length;
-    unsigned int new_size = old_size - (size + sizeof(struct BlkHeader));
-
-    block += size + sizeof(struct BlkHeader); // increment block pointer
-
-    defn_header((unsigned char*) block, new_size, block->next, 1);
-    defn_header((unsigned char*) out, size, (unsigned char*) block, 1);
+    out->length = size;
+    out->next = block;
+    out->magic = HEAP_MAGIC;
+    out->free = 1;
 
     return out;
 }
 
 void* malloc(unsigned int size) {
-    if(size == 0) {
-        return NULL;
+    if (size == 0) {
+        print_string("Attempt to malloc 0");
+        return 0;
     }
 
-    struct BlkHeader* header = parse_block(heap_begin);
+    size += sizeof(BlkHeader_t);
 
-    int i=0;
-    while(header != NULL) {
-        i++;
-        if(header->length >= size && header->free) {
-            goto found_block;
+    BlkHeader_t* header = (BlkHeader_t*)&heap_begin;
+    while (1) {
+        if (header == NULL) {
+            print_string("Kernel memory allocation error (out of blocks)!\n");
+            return 0;
         }
 
-        header = parse_block(header->next);
+        if (header->magic != HEAP_MAGIC) {
+            print_string(
+                "Kernel memory allocation error (invalid heap magic)!\n");
+            return 0;
+        }
+
+        if (header->length >= size && header->free) {
+            break;
+        }
+
+        header = header->next;
     }
 
-    return NULL;
-
-found_block:
-
-    if(header->length > size) {
+    if (header->length > size) {
         header = split(header, size);
     }
 
-    defn_header((unsigned char*) header, header->length, header->next, 0);
+    header->magic = HEAP_MAGIC;
+    header->free = 0;
 
-    unsigned char* out = ((unsigned char*) header) + sizeof(struct BlkHeader);
+    unsigned int out = ((unsigned int)header) + sizeof(BlkHeader_t);
 
-    return out;
+    // print_string("Malloc: ");
+    // printi(out, 16);
+    // print_string("\n");
+
+    return (void*)out;
 }
-
-// int defn_header(unsigned char* ptr, unsigned len, unsigned char* next, unsigned char free)
-void free(void* ptr) {
-    ptr -= sizeof(struct BlkHeader);
-
-    struct BlkHeader* header = parse_block(ptr);
-
-    if(header == NULL) {
-        error("Invalid kernel free");
-        return;
-    }
-
-    struct BlkHeader* next = parse_block(header->next);
-    unsigned int length = header->length;
-    if(next->free) {
-        length += next->length + sizeof(struct BlkHeader);
-    }
-
-    defn_header((unsigned char*) header, length, (unsigned char*) next, 1);
-}
-
-int memmgr_init() {
-    heap = (unsigned char*) &heap_begin_addr;
-    heap_size = (unsigned char*) &heap_end_addr - (unsigned char*) &heap_begin_addr;
-
-    heap_begin = heap;
-    heap_end = heap_begin + heap_size;
-
-    defn_header(heap_begin, heap_size-sizeof(struct BlkHeader), NULL, 1);
-
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
 
 void* realloc(void* ptr, unsigned int size) {
-    /*if (ptr == NULL)
+    if (ptr == NULL)
         return malloc(size);
     else if (size == 0) {
         free(ptr);
@@ -167,11 +132,29 @@ void* realloc(void* ptr, unsigned int size) {
         free(ptr);
     }
 
-    return (void*)new_header;*/
-    error("Realloc unimplemented!");
-    return NULL;
+    return (void*)new_header;
 }
 
+void free(void* ptr) {
+    // print_string("Free: ");
+    // printi(ptr, 16);
+    // print_string("\n");
+
+    BlkHeader_t* header =
+        (BlkHeader_t*)((unsigned int)ptr - sizeof(BlkHeader_t));
+
+    if (header->magic != HEAP_MAGIC) {
+        print_string("Invalid kernel free!\n");
+        return;
+    }
+
+    if (header->next->free) {
+        header->next = header->next->next;
+        header->length += header->next->length + sizeof(BlkHeader_t);
+    }
+
+    header->free = 1;
+}
 
 int a20_available() {
     return interrupt(0x15, 0x2403, 0, 0, 0);
