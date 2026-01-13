c9cd9cf950d11a663d4c7a10f0ddf67db79e0b59
Rhys Adams <rhys@therhys.co.uk>
2026-01-13 15:51:14 +0000

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,16 @@
 { pkgs ? import <nixpkgs> {} }:
+let
+  perlLibs = pkgs.perl.withPackages (p: [
+    p.FileFindRule
+    p.ConfigSimple
+  ]);
+in
   pkgs.mkShell {
     nativeBuildInputs = with pkgs.buildPackages; [
       qemu
       bochs
       docker
+
+      perlLibs
     ];
 }
